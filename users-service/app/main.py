7689c2564bffb7ef05fd0947224a87c16bc2c874
Muhammad Nahid <mdnahid22@gmail.com>
2025-08-11 15:26:33 +0600

    Refactor Kubernetes

--- users-service/app/main.py
+++ users-service/app/main.py
@@ -21,4 +21,4 @@ app.include_router(api_router)
 
 
 if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('app.main:app', host='0.0.0.0', port=8008, reload=True)
